a1f00d8c849b36572e7bcc81056a227ae7a856df
moonseopage1 <moon@seopage1.net>
2024-11-08 13:05:12 +0600

    :sparkles: Data Filtering: WHERE Clause, Logical Operators, and Comparison Operators

--- select.sql
+++ select.sql
@@ -66,3 +66,31 @@ SELECT * from students
 
 SELECT * from students
     WHERE (country = 'USA' OR country = 'Australia') AND age = 25;
+
+
+/*
+
+@Scaler functions
+UPPER() converts a string to uppercase
+LOWER() converts a string to lowercase
+CONCAT() concatenates two or more strings
+LENGTH() returns the length of a string
+
+@Aggregate functions
+AVG() Calculates the average value of a set of values
+MAX() Returns the maximum value in a set of values
+MIN() Returns the minimum value in a set of values
+SUM() Returns the sum of all values in a set of values
+COUNT() Returns the number of values in a set of values 
+
+*/
+
+SELECT concat(first_name,' ', last_name) FROM students;
+SELECT length(first_name) FROM students;
+
+SELECT avg(age) FROM students;
+SELECT max(age) FROM students;
+SELECT min(age) FROM students;
+SELECT sum(age) FROM students;
+SELECT count(*) FROM students;
+SELECT max(length(first_name)) FROM students;

628cbd1ac940d53fd074a194d5584a886320d495
moonseopage1 <moon@seopage1.net>
2024-11-22 16:23:03 +0600

    :sparkles:  Enforcing Referential Integrity: Behaviors During Data Deletion

--- foreignKey.sql
+++ foreignKey.sql
@@ -4,10 +4,41 @@ CREATE Table "user"(
     username VARCHAR(25) NOT NULL
 )
 
-CREATE Table "post"(
+CREATE Table post(
     id SERIAL PRIMARY KEY,
     title text NOT NULL,
-    user_id INTEGER REFERENCES "user"(id)
+    user_id INTEGER REFERENCES "user"(id) on delete set DEFAULT DEFAULT 2
 )
 
-SELECT * FROM "post"
+ALTER TABLE post
+    ALTER COLUMN user_id SET NOT NULL;
+
+
+INSERT INTO "user"(username) VALUES
+    ('akash'),
+    ('batash'),
+    ('sagor'),
+    ('nodi');
+
+
+INSERT INTO "post"(title,user_id) VALUES
+    ('post1',2),
+    ('post2',1),
+    ('post3',4),
+    ('post4',4);
+
+DROP Table "post";
+DROP Table "user";
+
+SELECT * FROM "user";
+
+SELECT * FROM "post";
+
+
+-- INSERT INTO "post"(title,user_id) VALUES('new post',NULL);
+
+INSERT INTO post(title,user_id) VALUES
+    ('null post',NULL);
+
+DELETE FROM "user" WHERE id = 4;
+-- DELETE FROM "post" WHERE id = 22;
